ab2d4ec398475f41dbcbb42482fd365e36af5976
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2024-01-14 12:13:09 +0100

    add landing component with tailwind.config file

---
+++ components/AppLanding.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <ULandingHero
+    title="Turn Your Vision into Reality"
+    description="Watch your dreams materialize before your eyes with us."
+  />
+</template>

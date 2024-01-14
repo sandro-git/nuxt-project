538439338ff41a5a5dce6756763b594441b9f025
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2024-01-14 01:35:25 +0100

    add ui and uipro

---
+++ components/AppHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+// import type { NavItem } from '@nuxt/content/dist/runtime/types'
+
+// const navigation = inject<Ref<NavItem[]>>('navigation', ref([]))
+
+const links = [{
+  label: 'Documentation',
+  icon: 'i-heroicons-book-open',
+  to: '/',
+}, {
+  label: 'Pro',
+  icon: 'i-heroicons-square-3-stack-3d',
+  to: '/',
+}, {
+  label: 'Releases',
+  icon: 'i-heroicons-rocket-launch',
+  to: 'https://github.com/nuxt/ui/releases',
+  target: '_blank',
+}]
+</script>
+
+<template>
+  <UHeader :links="links">
+    <template #logo>
+      <!-- <Logo class="w-auto h-6" /> -->
+    </template>
+
+    <template #right>
+      <!-- <UDocsSearchButton label="" /> -->
+
+      <UColorModeButton />
+
+      <UButton to="https://github.com/nuxt/ui" target="_blank" icon="i-simple-icons-github" color="gray" variant="ghost" />
+    </template>
+
+    <!-- <template #panel>
+      <UNavigationTree :links="mapContentNavigation(navigation)" />
+    </template> -->
+  </UHeader>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  extends: ['@nuxt/ui-pro'],
+  modules: ['@nuxt/ui'],
+  ui: {
+    icons: ['heroicons', 'simple-icons'],
+  },
 })
